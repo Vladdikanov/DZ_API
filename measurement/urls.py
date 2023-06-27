a18a287924241d4a5fe3f5415b72ed5d064fd528
Vladdikanov <116185949+Vladdikanov@users.noreply.github.com>
2023-06-27 12:49:45 +0700

    Update urls.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from measurement.views import CreateAndShowAPIView, UdateAndShowOne,CreateMeasurement
+from measurement.views import CreateAndShowAllAPIView, UdateAndShowOne, CreateMeasurement
 
 urlpatterns = [
-    path('sensors/',CreateAndShowAPIView.as_view()),
+    path('sensors/',CreateAndShowAllAPIView.as_view()),
     path('sensors/<pk>/',UdateAndShowOne.as_view()),
     path('measurements/',CreateMeasurement.as_view()),
     # TODO: зарегистрируйте необходимые маршруты
