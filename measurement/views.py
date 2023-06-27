68b562cf85bbb29b364378dfb08d12153def9987
Vladdikanov <116185949+Vladdikanov@users.noreply.github.com>
2023-06-27 12:48:56 +0700

    Update views.py

--- measurement/views.py
+++ measurement/views.py
@@ -1,16 +1,14 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
 
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView,CreateAPIView
-
-
-from measurement.models import Sensor,Measurement
-from .serializers import SensorSerializer, MeasurementSerializer, MeasurementAddSerializer
+from measurement.models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorAllSerializer
 
 
-class CreateAndShowAPIView(ListCreateAPIView):
+class CreateAndShowAllAPIView(ListCreateAPIView):
     queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
+    serializer_class = SensorAllSerializer
 
 class UdateAndShowOne(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
@@ -18,4 +16,4 @@ class UdateAndShowOne(RetrieveUpdateAPIView):
 
 class CreateMeasurement(CreateAPIView):
     queryset = Measurement.objects.all()
-    serializer_class = MeasurementAddSerializer
+    serializer_class = MeasurementSerializer
